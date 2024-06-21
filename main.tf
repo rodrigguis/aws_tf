827a59137b91ab1c0d4ff8c829848b93e94e6ee6
rodrigguis <rodrigguis@gmail.com>
2024-06-21 20:49:54 -0300

    add EOF

--- main.tf
+++ main.tf
@@ -18,8 +18,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
   key_name = "iac-alura"
+  user_data = <<-EOF
+  #!/bin/bash
+  cd /home/ubuntu
+  echo "<h1> Feito com Terraform</h1>" > index.html
+  nohup busybox httpd -f -p 8080 &
+  EOF
   tags = {
-    Name = "Primeira Instancia"
+    Name = "Teste aws"
   }
 }
 
