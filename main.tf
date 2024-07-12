0b52db9221d301136a49503f0c6b2dd9bd27170c
rodrigguis <rodrigguis@gmail.com>
2024-07-12 19:35:00 -0300

    fix: remove comments in main.tf

--- main.tf
+++ main.tf
@@ -17,14 +17,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
-  # key_name = "iac-alura"
   key_name = "iac-rodrigues"
-  # user_data = <<-EOF
-  # #!/bin/bash
-  # cd /home/ubuntu
-  # echo "<h1> Feito com Terraform</h1>" > index.html
-  # nohup busybox httpd -f -p 8080 &
-  # EOF
   tags = {
     Name = "terraform-ansible-python"
   }
